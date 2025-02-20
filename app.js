59b02ea31d3a34eadf866b838c7a1031cce1c787
marykhodaparast <maryam.kh1374@gmail.com>
2025-02-20 16:04:30 +0330

    fs completed

--- app.js
+++ app.js
@@ -56,14 +56,26 @@
 // };
 
 // console.log(currentOs);
-const path = require('path');
-console.log(path.sep);
+// const path = require('path');
+// console.log(path.sep);
 
-const filePath = path.join('/content', 'subfolder', 'test.txt');
-console.log(filePath);
+// const filePath = path.join('/content', 'subfolder', 'test.txt');
+// console.log(filePath);
 
-const base = path.basename(filePath);
-console.log(base);
+// const base = path.basename(filePath);
+// console.log(base);
 
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
-console.log(absolute);
+// const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
+// console.log(absolute);
+
+const { readFileSync, writeFileSync } = require("fs");
+// const fs = require("fs");
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
+
+writeFileSync(
+  "./content/result-sync.txt",
+  `Here is the result : ${first}, ${second}`,
+  { flag: "a" }
+);
+console.log(first, second);
